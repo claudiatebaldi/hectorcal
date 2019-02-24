c9d7a2a5cdde3e411fd0b7d725e324ff93bb9c98
Robert Link <robert.link@pnnl.gov>
2019-02-24 10:49:53 -0500

    Add writeup for experiment 1, part A.

--- R/util.R
+++ R/util.R
@@ -9,3 +9,19 @@
 calc_variance <- function(pca){
     cumsum(pca$sdev^2)/sum(pca$sdev^2)
 }
+
+
+#' Convert a list of metrosamp structures to a \code{\link[coda]{mcmc.list}}
+#' structure.
+#'
+#' @param mslist A list mof metrosamp structures
+#' @export
+metrosamp2coda <- function(mslist) {
+
+    coda::mcmc.list(
+        lapply(mslist, function(ms) {
+                   coda::mcmc(ms$samples)
+               })
+        )
+}
+
