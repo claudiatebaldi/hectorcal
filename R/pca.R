58c92a2290087d6eb245c14d29044e580d52df14
Robert Link <robert.link@pnnl.gov>
2019-02-28 16:40:00 -0500

    Adjust pca projection/unprojection functions to match new format.

--- R/pca.R
+++ R/pca.R
@@ -25,13 +25,20 @@ difference_between <- function(x, y){
 
 #' Project a data frame of climate data on to a principal componet basis
 #'
-#' @param climate_data an object of climate data, must contain value, year, experiment, and variable columns.
-#' It may not contain data for more years, variables, or experiments then in the principal_components argument.
-#' @param principal_components a \code{hectorcal} object that contains results from \code{prcomp} and meta data information.
-#' @return a list of three objects, rotation the PC loadings, scale the values used to scale the input climate data, and center the values used to center the climate data.
+#' @param climate_data a data frame of climate data.  The data must contain
+#' value, year, experiment, and variable columns.
+#' @param principal_components a \code{hectorcal} object that contains results
+#' from \code{prcomp} and meta data information.
+#' @param row_vector If \code{TRUE}, return the data as a row vector (a 1xN
+#' matrix)
+#' @return a list of three objects, rotation the PC loadings, scale the values
+#' used to scale the input climate data, and center the values used to center
+#' the climate data.
 #' @importFrom dplyr %>%
+#' @importFrom assertthat assert_that
 #' @export
-project_climate <- function(climate_data, principal_components){
+project_climate <- function(climate_data, principal_components, row_vector=TRUE)
+{
 
     # First check to make sure that the climate data contains the required columns
     check_columns(climate_data, c('value', 'year', 'experiment', 'variable'))
@@ -39,72 +46,70 @@ project_climate <- function(climate_data, principal_components){
     # Return the output strucutre
     obj <- list()
 
-    # Check to see that the climate data being read into the principal componets does not contain data the
-    # principal componet strucutre does not have data for.
-    missing_years      <- difference_between(climate_data$year, principal_components$meta_data$year)
-    missing_variable   <- difference_between(climate_data$variable, principal_components$meta_data$variable)
-    missing_experiment <- difference_between(climate_data$experiment, principal_components$meta_data$experiment)
+    ## Check to see that we have all the experiments we need.  We make this
+    ## check explicitly because getting all the experiments into a single data
+    ## frame probably requires assembling the data from several datasets and is
+    ## therefore easy to screw up.  For years and variables we assume that the
+    ## datasets are fairly uniform and therefore likely to have all the data
+    ## needed by default.
+    assert_that(all(principal_components$meta_data$experiment %in% climate_data$experiment))
 
-    if(length(missing_years)){stop('climate_data and principal_components are required to have the same years')}
-    if(length(missing_variable)){stop('climate_data and principal_components are required to have the same variables')}
-    if(length(missing_experiment)){stop('climate_data and principal_components are required to have the same experiments')}
+    ## Filter the data to include just the required data, and put the rows into
+    ## canonical order
+    cd <- dplyr::filter(climate_data,
+                        experiment %in% principal_components$meta_data$experiment,
+                        variable %in% principal_components$meta_data$variable,
+                        year %in% principal_components$meta_data$year) %>%
+      dplyr::arrange(experiment, variable, year)
 
-    # Match the cliamte data with the pc center and scales, this ensures that the climate data will be centered and
-    # scaled properly in the following step.
-    climate_data %>%
-        dplyr::mutate(col = paste0('X', year, '_', variable, '_', experiment)) %>%
-        dplyr::full_join(tibble::tibble(center = principal_components$center,
-                                        col = names(principal_components$center)), by = 'col') %>%
-        dplyr::full_join(tibble::tibble(scale = principal_components$scale,
-                                        col = names(principal_components$scale)), by = 'col') %>%
-        dplyr::full_join(tibble::tibble(col = row.names(principal_components$rotation),
-                                        keep = 1), by = 'col')->
-        tibble_value_scale_center
+    assert_that(nrow(cd) == nrow(principal_components$rotation))
 
-    # If there are any NAs throw an error, we expect there to be a 1:1 match.
-    if(any(is.na(tibble_value_scale_center))) { stop('There is a mismatch between the information in the climate_data and the principal_components arguments.') }
+    ## Extract the data, center, and scale
+    vp <- (matrix(cd$value, nrow=1) - principal_components$center) / principal_components$scale
 
-    # Scale and center the new data
-    newdata <- (tibble_value_scale_center$value - tibble_value_scale_center$center) / tibble_value_scale_center$scale
-    obj[['rotation']] <- newdata %*% principal_components$rotation
+    p <- vp %*% principal_components$rotation
 
-    # Scales
-    scale             <- tibble_value_scale_center$scale
-    names(scale)      <- tibble_value_scale_center$col
-    obj[['scale']]    <- scale
-
-    # Center
-    center            <- tibble_value_scale_center$center
-    names(center)     <- tibble_value_scale_center$col
-    obj[['center']]   <- center
-
-    # Return the object
-    obj
+    if(row_vector) {
+        p
+    }
+    else {
+        as.vector(p)
+    }
 }
 
 
 #' Reconstruct climate data from projected climate data and prcinicpal componetns
 #'
-#' @param projected_climate an object returned by \code{project_climate}
-#' @param principal_components a \code{hectorcal} object that contains results from \code{prcomp} and meta data information.
-#' @return a data frame of reconstructed cliamte data containing the following columns, year, value, experiment, and variable
-#' @import dplyr
-#' @import tidyr
+#' @param projected_climate a vector or row-vector of PC coordinates
+#' @param principal_components a \code{hectorcal} object that contains results
+#' from \code{prcomp} and meta data information.
+#' @param ncomp Number of components to keep in the reconstruction.  Default is
+#' to keep them all.
+#' @return a data frame of reconstructed cliamte data containing the following
+#' columns, year, value, experiment, and variable
+#' @importFrom dplyr %>%
 #' @export
-reconstruct_climate <- function(projected_climate, principal_components){
+reconstruct_climate <- function(projected_climate, principal_components, ncomp=NA)
+{
+    if(is.na(ncomp)) {
+        ncomp <- ncol(principal_components$rotation)
+    }
 
-    # Revert the information back to original coordinate system
-    uncentered_unscaled_data <- colSums(t(principal_components$rotation) * as.vector(projected_climate$rotation))
+    if(is.vector(projected_climate)) {
+        projected_climate <- matrix(projected_climate, nrow=1)
+    }
 
-    # Recenter and scale the data
-    value <- c(uncentered_unscaled_data * principal_components$scale) + principal_components$center
+    p <- projected_climate[ , 1:ncomp, drop=FALSE]
+    rinv <- t(principal_components$rotation)[1:ncomp, , drop=FALSE]
+
+    vp <- p %*% rinv
+
+    v <- vp*principal_components$scale + principal_components$center
 
     # Format information in data frame
-    data.frame(value = as.vector(value), col = names(value)) %>%
-        tidyr::separate(col, into = c('year', 'variable', 'experiment')) %>%
-        dplyr::mutate(year = as.integer(gsub('X', '', year))) %>%
+    data.frame(value = as.vector(v), col = colnames(rinv)) %>%
+        tidyr::separate(col, into = c('experiment', 'variable', 'year'), sep='\\.') %>%
+        dplyr::mutate(year = as.integer(year)) %>%
         dplyr::select(year, value, variable, experiment)
-
 }
-
 
--- tests/testthat/test_pca.R
+++ tests/testthat/test_pca.R
@@ -16,36 +16,71 @@ test_that('Helper functions work', {
 test_that('project_climate works', {
 
     # Load the test climate data and principal components.
-    test_PCA <- get(load(list.files(system.file('tests/testthat', package = 'hectorcal'), 'PCA_climate_test.rda', full.names = TRUE)))
-    test_PC  <- get(load(list.files(system.file('tests/testthat', package = 'hectorcal'), 'PC_climate_test.rda', full.names = TRUE)))
+    test_climdata <- readRDS('test_climdata.rds')
+    test_pca <- readRDS('test_pca.rds')
 
-    # Pull out data from a single run to use as the input climate data.
-    climate_data <- test_PCA[test_PCA$runid == 2, ]
+    p1 <- rep(0, 50); p1[1] <- 1        # expected result for runid==1
+    p2 <- rep(0, 50); p2[1] <- 1; p2[2] <- 0.5 # expected result for runid==2
+    p3 <- rep(NA, 50)                          # runid==3 is real model output,
+                                        # so we don't have an expected projection
+                                        # for it.
+    expected_proj <- list(p1, p2, p3)
+    for(i in 1:3) {
+        ## Pull out data from a single run to use as the input climate data.
+        climate_data <- test_climdata[test_climdata$runid == i, ]
 
-    # Project the cliamte data on to the principal componet basis and then reconstruct the original climate data.
-    projected_rslt <- project_climate(climate_data = climate_data, principal_components = test_PC)
-    recon_climate  <- reconstruct_climate(projected_rslt, principal_components = test_PC)
+        ## Project the cliamte data on to the principal componet basis and then
+        ## reconstruct the original climate data.
+        projected_rslt <- project_climate(climate_data = climate_data,
+                                          principal_components = test_pca, row_vector=FALSE)
+        if(!any(is.na(expected_proj[[i]]))) {
+            expect_equal(projected_rslt, expected_proj[[i]],
+                         info=paste("Didn't get expected projection coefs for runid=",i))
+        }
+        recon_climate  <- reconstruct_climate(projected_rslt, principal_components = test_pca)
 
-    # We expect that the reconstructed climate data and the input climare data are equal to one another.
-    expect_equal(recon_climate$value, climate_data$value)
+        ## We expect that the reconstructed climate data and the input climare
+        ## data are equal to one another.  However, we need to ensure that the
+        ## input data is in canonical order
+        cd <- dplyr::arrange(climate_data, experiment, variable, year)
+        expect_equal(recon_climate$value, cd$value,
+                     info=paste("Reconstructed climate not equal to input for runid=",i),
+                     tolerance=1e-3)    # The round-trip procedure can induce a
+                                        # fair bit of roundoff error.
+    }
 
+    ## Verify that the row-vector version of project_climate works
+    cd <- test_climdata[test_climdata$runid==1, ]
+    p <- project_climate(cd, test_pca)
+    expect_equal(dim(p), c(1, 50))
+    expect_equal(as.vector(p), p1)
+    rclim <- reconstruct_climate(p, test_pca)
+    expect_equal(rclim$value, cd$value)
+
+    ## Verify that setting ncomp in reconstruct_climate works
+    c1 <- reconstruct_climate(p2, test_pca)
+    c2 <- reconstruct_climate(p2, test_pca, 1)
+    vdiff <- (c1$value - c2$value) / test_pca$scale
+    expect_equivalent(vdiff, 0.5*test_pca$rotation[ ,2])
+
+    test_PC <- test_pca    # avoid having to change names in all the tests below.
     # Make sure that the project climate function throws an error, when the climate input data and the principal components
     # do not have the same data structure.
     missing_var <- climate_data
-    missing_var$variable <- 'co2'
-    expect_error(project_climate(missing_var, test_PC), 'climate_data and principal_components are required to have the same variables')
+    missing_var <- dplyr::filter(missing_var, variable == 'co2')
+    expect_error(project_climate(missing_var, test_PC), 'nrow.cd. not equal to nrow.principal_components')
 
     missing_years <- climate_data
     missing_years$year <- c(climate_data$year + 10)
-    expect_error(project_climate(missing_years, test_PC), 'climate_data and principal_components are required to have the same years')
+    expect_error(project_climate(missing_years, test_PC), 'nrow.cd. not equal to nrow.principal_components')
 
     missing_experiment            <- climate_data
-    missing_experiment$experiment <- c('rcp26', 'rcp85', 'rcp26', 'rcp26', 'rcp85', 'rcp26', 'rcp26', 'rcp85', 'rcp26', 'rcp45', 'rcp45')
-    expect_error(project_climate(missing_experiment, test_PC), 'climate_data and principal_components are required to have the same experiments')
+    missing_experiment$experiment <- c('rcp85')
+    expect_error(project_climate(missing_experiment, test_PC), 'experiment %in% climate_data')
 
     # pc missing columns
     missing_pc_rows <- test_PC
     missing_pc_rows$rotation <- missing_pc_rows$rotation[1:3, ]
-    expect_error(project_climate(climate_data, missing_pc_rows), 'There is a mismatch between the information in the climate_data and the principal_components arguments.')
+    expect_error(project_climate(climate_data, missing_pc_rows), 'nrow.cd. not equal to nrow.principal_components')
 
 })
