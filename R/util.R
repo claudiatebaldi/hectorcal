fb1d229900b5c840d15fa2201c7eb02c5cebbd97
Robert Link <robert.link@pnnl.gov>
2019-03-24 19:32:23 -0400

    Support functions and analysis for experiment 2A.

--- R/util.R
+++ R/util.R
@@ -25,37 +25,3 @@ metrosamp2coda <- function(mslist) {
         )
 }
 
-
-#' Get a data frame of gates for an experiment
-#'
-#' Find the minimum and maximum attested values by year for a climate variable
-#' in the CMIP5 output.  Values are returned at the beginning, end, and median
-#' of the requested period.
-#'
-#' For historical experiments, the final year can be no later than 2005.  For
-#' future experiments, the starting year can be no earlier than 2006.
-#' The \code{minyear} and \code{maxyear} parameters will be adjusted
-#' automatically to these limits.
-#'
-#' @param expt Experiment to retrieve ('historical', 'rcp26', 'rcp45', etc.)
-#' @param var Variable to retrieve ('tas' or 'co2')
-#' @param minyear Earliest year to retrieve.
-#' @param maxyear Latest year to retrieve.
-#' @return Data frame with the matching rows from the
-#' \code{\link{esm_comparison}} dataset.
-#' @export
-get_gates <- function(expt, var, minyear=1861, maxyear=2100) {
-    if(grepl('[Hh]istorical', expt)) {
-        maxyear <- min(c(2005, maxyear))
-    }
-    else {
-        minyear <- max(c(2006, minyear))
-    }
-
-    exptdata <- filter(esm_comparison, experiment==expt, variable==var,
-                       year <= maxyear, year >= minyear)
-    yrs <- c(min(exptdata$year),
-             max(exptdata$year[exptdata$year <= median(exptdata$year)]),
-             max(exptdata$year))
-    filter(exptdata, year %in% yrs)
-}
